ce7d0509c0f669f6d5bfe4b98a0d712c634ded9e
Abdy Franco <yosoy@abdyfran.co>
2023-06-21 16:53:08 -0600

    Initial commit

---
+++ lib/paypal_checkout_response.php
@@ -0,0 +1,98 @@
+<?php
+/**
+ * PayPal Checkout API Response
+ *
+ * @copyright Copyright (c) 2023, Phillips Data, Inc.
+ * @license http://opensource.org/licenses/mit-license.php MIT License
+ * @package paypal_checkout.commands
+ */
+class PaypalCheckoutResponse
+{
+    private $status;
+    private $raw;
+    private $response;
+    private $errors;
+    private $headers;
+
+    /**
+     * PaypalCheckoutResponse constructor.
+     *
+     * @param array $api_response The API response
+     */
+    public function __construct(array $api_response)
+    {
+        $this->raw = $api_response['content'];
+        $this->response = json_decode($api_response['content']);
+        $this->headers = $api_response['headers'];
+
+        $this->status = '400';
+        if (isset($this->headers[0])) {
+            $status_parts = explode(' ', $this->headers[0]);
+            if (isset($status_parts[1])) {
+                $this->status = $status_parts[1];
+            }
+        }
+
+        // Set errors
+        $this->errors =  [];
+        if (isset($this->response->errors)) {
+            foreach ($this->response->errors as $error) {
+                $this->errors[] = $error->msg;
+            }
+        } elseif (isset($this->response->error)) {
+            $this->errors = [
+                $this->response->error => $this->response->error_description ?? $this->response->error
+            ];
+        }
+    }
+
+    /**
+     * Get the status of this response
+     *
+     * @return string The status of this response
+     */
+    public function status()
+    {
+        return $this->status;
+    }
+
+    /**
+     * Get the raw data from this response
+     *
+     * @return string The raw data from this response
+     */
+    public function raw()
+    {
+        return $this->raw;
+    }
+
+    /**
+     * Get the data response from this response
+     *
+     * @return mixed The data response from this response
+     */
+    public function response()
+    {
+        return $this->response;
+    }
+
+    /**
+     * Get any errors from this response
+     *
+     * @return array The errors from this response
+     */
+    public function errors()
+    {
+        return $this->errors;
+    }
+
+    /**
+     * Get the headers returned with this response
+     *
+     * @return string The headers returned with this response
+     */
+    public function headers()
+    {
+        return $this->headers;
+    }
+}
